eee7ba77cb399ef3b7e52c152b6f3236263349db
Rohit Tiwari <166986989+Dedrknex8@users.noreply.github.com>
2025-05-09 10:31:25 +0530

    Fetching Single Post

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,12 +1,16 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param, ParseIntPipe } from '@nestjs/common';
 import { PostsService } from './posts.service';
-
-@Controller('v1')
+import {Post as PostInterface}  from  '../interface/post.interface'
+@Controller('posts')
 export class PostsController {
-    constructor(private readonly postService : PostsService){}
+    constructor(private readonly postService : PostsService ){}
         @Get()
         findAll(){
             return this.postService.findAll()
+        }
+        @Get(':id')
+        findOne(@Param('id', ParseIntPipe)id : Number): PostInterface{
+            return this.postService.findSinglePost(id)
         }
 
         
--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -1,5 +1,6 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import {Post} from '../interface/post.interface'
+import { single } from 'rxjs';
 @Injectable()
 export class PostsService {
     private posts : Post[]= [
@@ -14,5 +15,12 @@ export class PostsService {
 
     findAll() : Post[]{
         return this.posts
+    }
+    findSinglePost(id:Number) : Post{
+        const singlePost = this.posts.find(post=> post.id === id)
+        if(!singlePost){
+            throw new NotFoundException(`Post with this ${id} is not found`);
+        }
+        return singlePost
     }
 }
