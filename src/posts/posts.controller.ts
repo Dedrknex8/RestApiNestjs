5bda325977c5b2c86eb1fc57f37655a4a5a81fba
Rohit Tiwari <166986989+Dedrknex8@users.noreply.github.com>
2025-05-11 21:14:23 +0530

    some intial changes

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param, ParseIntPipe } from '@nestjs/common';
+import { Body, Controller, Get, HttpCode, HttpStatus, Param, ParseIntPipe, Post } from '@nestjs/common';
 import { PostsService } from './posts.service';
 import {Post as PostInterface}  from  '../interface/post.interface'
 @Controller('posts')
@@ -12,6 +12,13 @@ export class PostsController {
         findOne(@Param('id', ParseIntPipe)id : Number): PostInterface{
             return this.postService.findSinglePost(id)
         }
+        @Post()
+        @HttpCode(HttpStatus.CREATED)
+        create(@Body() createPostData : Omit<PostInterface,'id' | 'createdAt'>) : PostInterface{
+            return this.postService.createPost(createPostData)
+        }
+
+
 
         
         
