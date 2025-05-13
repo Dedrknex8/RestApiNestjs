25803222c9d3b1c9ac002a3c6646227a335c0e24
Rohit Tiwari <166986989+Dedrknex8@users.noreply.github.com>
2025-05-13 19:31:45 +0530

    Update post controller

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, HttpCode, HttpStatus, Param, ParseIntPipe, Post } from '@nestjs/common';
+import { Body, Controller, Get, HttpCode, HttpStatus, Param, ParseIntPipe, Post, Put } from '@nestjs/common';
 import { PostsService } from './posts.service';
 import {Post as PostInterface}  from  '../interface/post.interface'
 @Controller('posts')
@@ -17,6 +17,12 @@ export class PostsController {
         create(@Body() createPostData : Omit<PostInterface,'id' | 'createdAt'>) : PostInterface{
             return this.postService.createPost(createPostData)
         }
+
+        @Put(':id')
+        @HttpCode(200)
+        update(@Param('id',ParseIntPipe)id:number,@Body() updatePostData: Partial<Omit<PostInterface,'id' | 'createdAt'>>): PostInterface{
+            return this.postService.updatePost(id,updatePostData)
+        } //ParseIntPipe is used to convert string into integer
 
 
 
