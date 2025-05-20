127201b0cf03ada774d20fc1a3fc245167ba8b44
Rohit Tiwari <166986989+Dedrknex8@users.noreply.github.com>
2025-05-20 19:32:33 +0530

    Implemented Role Gurad for RBAC

---
+++ src/auth/guards/role.guard.ts
@@ -0,0 +1,39 @@
+import { CanActivate, ExecutionContext, ForbiddenException } from "@nestjs/common";
+import { Reflector } from "@nestjs/core";
+import { Observable } from "rxjs";
+import { Role } from "../entity/user.entities";
+import { ROLES_KEY } from "../Decorators/roles.decorators";
+
+
+export class RolesGuards implements CanActivate{
+    constructor(private reflector : Reflector){}
+
+    canActivate(context: ExecutionContext): boolean  {
+        
+        const requiredRoles = this.reflector.getAllAndOverride<Role[]>(
+            ROLES_KEY, [
+                context.getHandler(), //method level metadata
+                context.getClass() //class level metadata
+            ]
+        )
+
+        //if requiredRoles is not requried that means the route is accessible to any user
+        if(!requiredRoles){
+            return true
+        }
+
+        //get current user
+
+        const {user} = context.switchToHttp().getRequest()
+
+        if(!user){
+            throw new ForbiddenException('User is not authenticated')
+        }
+        const hasRequiredRole = requiredRoles.some(role=> user.role === role)
+        
+        if(!hasRequiredRole){
+            throw new ForbiddenException('Invalid permission')
+        }
+        return true
+    }
+}
